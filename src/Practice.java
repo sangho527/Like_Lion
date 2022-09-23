d743b3f35337f309f4ba08be90783bef584833c9
PC <sy980527@naver.com>
2022-09-23 17:39:32 +0900

    접근제한자

---
+++ src/Practice.java
@@ -0,0 +1,10 @@
+public class Practice {
+    private int a;  //은닉화
+    public void setA(int n){
+        a = n;
+    }
+    public int getA(){
+        return a;
+    }
+}
+
---
+++ src/practice2.java
@@ -0,0 +1,9 @@
+public class practice2 {
+    public static void main(String[] args) {
+        Practice v = new Practice();
+        //System.out.println(v.a); //호출불가
+
+        v.setA(100);
+        System.out.println(v.getA());
+    }
+}
