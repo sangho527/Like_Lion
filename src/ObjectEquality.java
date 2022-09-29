7e968ee7fe8543c2975989c2a89ab03d3597da00
PC <sy980527@naver.com>
2022-09-29 13:19:18 +0900

    객체 내용비교

---
+++ src/ObjectEquality.java
@@ -0,0 +1,42 @@
+class INum {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public INum(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof INum) {
+            INum inum = (INum) obj;
+            if (this.number == inum.getNumber()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+public class ObjectEquality {
+    public static void main(String[] args) {
+
+        INum num1 = new INum(10);
+        INum num2 = new INum(12);
+        INum num3 = new INum(10);
+
+        if(num1.equals(num2))
+            System.out.println("num1, num2 내용 동일하다.");
+        else
+            System.out.println("num1, num2 내용 다르다.");
+
+        if(num1.equals(num3))
+            System.out.println("num1, num3 내용 동일하다.");
+        else
+            System.out.println("num1, num3 내용 다르다.");
+    }
+}
