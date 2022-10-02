6935ea6ed40800f5235f9dbc26ab1776e9f4bfab
PC <sy980527@naver.com>
2022-10-03 00:41:45 +0900

    어절개수 측정

--- src/StringEX3.java
+++ src/StringEX3.java
@@ -5,11 +5,11 @@ public class StringEX3 {
         Scanner sc = new Scanner(System.in);
 
 
-        while(true) {
+        while(true) {           // while문 사용
             System.out.print("문장을 입력하시오: ");
             String str = sc.nextLine();
 
-            if (str.equals("그만")) {
+            if (str.equals("그만")) { //break문을 사용해서 탈출
                 System.out.println("종료합니다...");
                 break;
             }
