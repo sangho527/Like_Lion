1c58711e2cb979bedb1f02aa89116b618ca32544
sangho527 <sy980527@naver.com>
2022-10-04 17:40:45 +0900

    221004
    codeup과제

---
+++ src/codeup1010.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // 스캐너 사용
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
