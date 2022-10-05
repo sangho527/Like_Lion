2788de2bfbdcef02483c063434b4df2e094b224a
sangho527 <sy980527@naver.com>
2022-10-05 09:23:40 +0900

    221005 SwapExercise

---
+++ src/SwapExercise.java
@@ -0,0 +1,15 @@
+public class SwapExercise {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        int temp = a;
+
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
